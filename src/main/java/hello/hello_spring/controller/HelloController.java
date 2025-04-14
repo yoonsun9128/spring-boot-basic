90684f0e5d641f9ef719de63f99d4c5782c64afc
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2025-04-15 00:03:08 +0900

    init

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,29 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//왜 사용하는거야?? controller
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "Azalea!!!공부했어@@@@@@@@" );
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+}
