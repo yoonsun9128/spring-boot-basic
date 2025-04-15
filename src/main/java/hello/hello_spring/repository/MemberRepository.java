489820d060ee366c22ee45b90837c11e73704eb9
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2025-04-15 22:37:40 +0900

    Member Repository add

---
+++ src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member); //회원 저장
+    Optional<Member> findById(Long id); //null 처리하는 방법
+    Optional<Member> findByName(String name);
+    List<Member> findAll(); //저장된 리스트 모두 변환
+}
---
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+    //저장담을 메모리, 단순한 형태이기 때문에 hashmap이용
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L; //동시성 문제를 고려해서 어텀롱을 사용한다
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); //id를 세팅을 하고
+        store.put(member.getId(), member); //store에 저장을해준다
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); //객체가 없을경우 null에 대한 처리를 위해
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); //하나라도 찾는것
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
