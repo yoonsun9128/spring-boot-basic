ba5fa9b384440e48fe489f8a95ec3d5246032cab
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2025-04-15 21:58:56 +0900

    api json send

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -22,8 +22,30 @@ public class HelloController {
     }
 
     @GetMapping("hello-string")
+    //http body에 내가 직접 값을 넣어주겠다.
     @ResponseBody
     public String helloString(@RequestParam("name") String name) {
         return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    //객체를 넘기는 api json 형식으로 보내준다
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello(); //새로운 객체를 생성
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
